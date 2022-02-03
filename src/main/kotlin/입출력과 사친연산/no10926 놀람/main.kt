abffcf9e7b003822de7cda9f8bbd6b6c00fdcfcb
charmacist <emkang2048@gmail.com>
2022-02-03 14:10:15 +0900

    study day 14 입출력과 사칙연산 추가

---
+++ src/main/kotlin/입출력과 사친연산/no10926 놀람/main.kt
@@ -0,0 +1,5 @@
+package `입출력과 사친연산`.`no10926 놀람`
+
+fun main() {
+    println(readLine() + "??!")
+}
---
+++ src/main/kotlin/입출력과 사친연산/no18108 1998년생인 내가 태국에서는 2541년생/main.kt
@@ -0,0 +1,5 @@
+package `입출력과 사친연산`.`no18108 1998년생인 내가 태국에서는 2541년생`
+
+fun main() {
+    println(readLine()!!.toInt() - 543)
+}
